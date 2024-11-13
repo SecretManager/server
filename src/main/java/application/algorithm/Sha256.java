77676ca46c6add3f4376eed8c3623c72ae552ee0
Donghun Shin <huipulci1@naver.com>
2024-11-13 13:39:31 +0900

    암호화 비즈니스 로직 구현 (#4)

---
+++ src/main/java/application/algorithm/Sha256.java
@@ -0,0 +1,33 @@
+package application.algorithm;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256 {
+
+    private static final String HEX_FORMAT = "%02x";
+
+    public static String encrypt(String text) {
+        try {
+            // MessageDigest 는 thread-safe 하지 않음
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(text.getBytes());
+            return bytesToHex(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format(HEX_FORMAT, b));
+        }
+        return builder.toString();
+    }
+
+    public static boolean check(String text, String expectedHash) {
+        String encryptedText = encrypt(text);
+        return encryptedText.equals(expectedHash);  // 해시값이 일치하면 true 반환
+    }
+}
---
+++ src/main/java/application/encryptor/Encryptor.java
@@ -0,0 +1,76 @@
+package application.encryptor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class Encryptor {
+
+    private static final int NO_STREAM_DATE = -1;
+    private static final int ENCODE_BLOCK_SIZE = 1024;
+    private static final int AES_GCM_TAG_LENGTH = 128;
+    private static final String AES_GCM_TRANSFORMATION = "AES/GCM/NoPadding";
+
+    private final Sha256SecretKeyGenerator generator;
+
+    public byte[] encrypt(InputStream inputStream, String plainKey, Nonce nonce) {
+        SecretKey key = generator.generateAESKey(plainKey);
+        Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, key, nonce);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            outputStream.write(nonce.getBytes());
+            applyCipher(inputStream, cipher, outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public byte[] decrypt(InputStream inputStream, String plainKey) throws Exception {
+        SecretKey key = generator.generateAESKey(plainKey);
+        // 암호화된 데이터에서 nonce(12바이트)를 분리
+        Nonce nonce = Nonce.extractFromPrefix(inputStream);
+        Cipher cipher = getCipher(Cipher.DECRYPT_MODE, key, nonce);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            applyCipher(inputStream, cipher, outputStream);
+            return outputStream.toByteArray();
+        }
+    }
+
+    private Cipher getCipher(int encryptMode, SecretKey key, Nonce nonce) {
+        try {
+            GCMParameterSpec gcmSpec = new GCMParameterSpec(AES_GCM_TAG_LENGTH, nonce.getBytes());
+            Cipher cipher = Cipher.getInstance(AES_GCM_TRANSFORMATION);
+            cipher.init(encryptMode, key, gcmSpec);
+            return cipher;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void applyCipher(
+            InputStream inputStream,
+            Cipher cipher,
+            ByteArrayOutputStream outputStream
+    ) throws Exception {
+        byte[] buffer = new byte[ENCODE_BLOCK_SIZE];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != NO_STREAM_DATE) {
+            byte[] outputBytes = cipher.update(buffer, 0, bytesRead);
+            writeToOutputStream(outputBytes, outputStream);
+        }
+        byte[] finalBytes = cipher.doFinal();
+        writeToOutputStream(finalBytes, outputStream);
+    }
+
+    private void writeToOutputStream(byte[] outputBytes, ByteArrayOutputStream outputStream) throws Exception {
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+    }
+}
